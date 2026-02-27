e0e402009073271099bea4a317551b2e6caa16f1
Raihan Sharif <raihansharif92@gmail.com>
2026-02-27 21:04:12 +0000

    complete receipt exercise

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,18 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+function printReceipt(order) {
+  console.log(`${"QTY".padEnd(8)}${"ITEM".padEnd(20)}TOTAL`);
+  let total = 0;
+  order.forEach((item) => {
+    const { quantity, itemName, unitPricePence } = item;
+    console.log(
+      `${String(quantity).padEnd(8)}${String(itemName).padEnd(20)}${((quantity * unitPricePence) / 100).toFixed(2)}`
+    );
+    total += quantity * unitPricePence;
+  });
+  console.log(`\nTotal: ${(total / 100).toFixed(2)}`);
+}
+
+printReceipt(order);
