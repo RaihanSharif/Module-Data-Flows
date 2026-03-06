d09f10db0d09a1f88646ea3319418e6bf1e3b283
Raihan Sharif <raihansharif92@gmail.com>
2026-03-06 19:38:50 +0000

    Fix: Render all books on page load and when adding new books using form

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -38,7 +38,7 @@ function submit() {
     return false;
   } else {
     let book = new Book(title.value, title.value, pages.value, check.checked);
-    library.push(book);
+    myLibrary.push(book);
     render();
   }
 }
@@ -89,12 +89,12 @@ function render() {
     });
 
     //add delete button to every row and render again
-    let delButton = document.createElement("button");
-    delBut.id = i + 5;
-    deleteCell.appendChild(delBut);
-    delBut.className = "btn btn-warning";
-    delBut.innerHTML = "Delete";
-    delBut.addEventListener("clicks", function () {
+    let delBtn = document.createElement("button");
+    delBtn.id = i + 5;
+    deleteCell.appendChild(delBtn);
+    delBtn.className = "btn btn-warning";
+    delBtn.innerHTML = "Delete";
+    delBtn.addEventListener("clicks", function () {
       alert(`You've deleted title: ${myLibrary[i].title}`);
       myLibrary.splice(i, 1);
       render();
