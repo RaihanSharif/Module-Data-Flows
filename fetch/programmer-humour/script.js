4957f2e0e1102c1ce31a801520d61019e36ceae1
Raihan Sharif <raihansharif92@gmail.com>
2026-05-08 17:39:19 +0100

    Minimal solution

---
+++ fetch/programmer-humour/script.js
@@ -0,0 +1,26 @@
+async function fetchXKCDComic() {
+  const url = "https://xkcd.now.sh/?comic=latest";
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
+  }
+  const result = await response.json();
+  return result;
+}
+
+async function displayComic() {
+  const comicDisplay = document.querySelector("img");
+  const errorDisplay = document.querySelector("p");
+
+  try {
+    const data = await fetchXKCDComic();
+    console.log(data);
+    comicDisplay.src = data.img;
+    comicDisplay.alt = data.alt;
+    comicDisplay.title = data.title;
+  } catch (error) {
+    errorDisplay.textContent = error.message;
+  }
+}
+
+document.onload = displayComic();
