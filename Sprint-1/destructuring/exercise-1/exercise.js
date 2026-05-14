5cd7bafcdd0a0ba5e7e8a4249a6f64a71a11a5c1
Raihan Sharif <raihansharif92@gmail.com>
2026-05-14 17:57:56 +0100

    removed default values

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -6,11 +6,7 @@ const personOne = {
 
 // Update the parameter to this function to make it work.
 // Don't change anything else.
-function introduceYourself({
-  name = "NO NAME",
-  age = "NO AGE",
-  favouriteFood = "NO FOOD",
-}) {
+function introduceYourself({ name, age, favouriteFood }) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
